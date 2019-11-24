909b8d39e7713da7065aa04e20963bb7485593ea
ferrriii <57658129+ferrriii@users.noreply.github.com>
2019-11-24 13:00:54 +0330

    Make book card texts width responsive

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -20,7 +20,7 @@
           </router-link>
         </div>
         <div class="flex-grow overflow-y-auto bt bw1 b--black-30" style="height:0px; min-height:50px">
-          <Card v-for="book in collections" :key="book.id" :title="ellipsis(book.title)" :subtitle="ellipsis(book.lastText)" :book="book" :img="`https://avatars.dicebear.com/v2/initials/${book.title}.svg`">
+          <Card v-for="book in collections" :key="book.id" :title="ellipsisCardText(book.title)" :subtitle="ellipsisCardText(book.lastText)" :book="book" :img="`https://avatars.dicebear.com/v2/initials/${book.title}.svg`">
             <div v-if="book.subscribed" @click="unsubscribe(book)" class="f6 dib br-pill ba b--black-10 dim cursor-hand primary pv1 ph2 pv2-ns ph3-ns mh1" type="submit">Unsub</div>
             <div v-if="!book.subscribed" @click="subscribe(book)" class="f6 dib bg-primary br-pill ba b--black-10 dim cursor-hand white pv1 ph2 pv2-ns ph3-ns mh1" type="submit">Subscribe</div>
             <router-link :to="{ name: 'publish', params: { book }}">
@@ -69,7 +69,9 @@ export default {
     }
   },
   methods: {
-    ellipsis,
+    ellipsisCardText (str) {
+      return ellipsis(str, 19 + (window.screen.width - 375)/ 20)
+    },
     async subscribe (book) {
       let response = await this.$axios.post('/subscribe', { book: book.id })
       if (response.status === 204) {
